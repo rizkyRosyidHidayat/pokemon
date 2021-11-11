71616bd9a6df1f686c77a8e0c2381639983eb64a
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-11-12 01:01:29 +0700

    fixing index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import NextLink from 'next/link'
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import About from 'component/About'
-import Card from 'component/Card'
 import ListPokemonByType from 'component/ListPokemonByType';
 const Home: NextPage = () => {
   return (
-    <Router>
-      <>
-        <ListPokemonByType></ListPokemonByType>
-      </>
-    </Router>
+    <>
+      <Head>
+        <title>PokeLab</title>
+        <meta name="description" content="PokeLab is apps pokemon" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Router>
+          <>
+            <ListPokemonByType></ListPokemonByType>
+          </>
+        </Router>
+      </main>
+    </>
   )
 }
 
