47f3d1d557ae0858d560fadc0fed9a97f6c4fde3
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-11-11 07:31:14 +0700

    setting redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../store"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div suppressHydrationWarning>
-      {typeof window === 'undefined' ? null : <Component {...pageProps} />}
+      {typeof window === "undefined" ? null : (
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      )}
     </div>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
