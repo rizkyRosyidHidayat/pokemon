3868dc1e439e31d6b32ef247473a66a00890cbbb
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-11-13 15:13:02 +0700

    add success catch pokemon modal

---
+++ component/styles/detail_pokemon/modal_catch_pokemon/Success.ts
@@ -0,0 +1,33 @@
+export const succes = `
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  & div.wrapper-img{
+    margin: 0 auto -2rem auto;
+    width: 10rem;
+    height: 10rem;
+    display: block;
+    position: relative;
+  }
+
+  & form.card{
+    background-color: #fefefe;
+    padding: 2.5rem 1.5rem 1rem 1.5rem;
+    border-radius: 1.5rem;
+    width: 100%;
+  }
+
+  & form input.input-nickname{
+    border: 3px solid #0e0e0e;
+    padding: 1rem 1.25rem;
+    border-radius: 0.5rem;
+    width: 100%;
+    text-align: center;
+    font-size: 1rem;
+  }
+
+  & form input.input-nickname:focus{
+    outline: none;
+  }
+`
---
+++ function/catch_pokemon/index.ts
@@ -0,0 +1,45 @@
+export function catchingPokemon(): boolean {
+  let res = true;
+  const opportunity = Math.round(Math.random() * 1);
+
+  if (opportunity === 1) {
+    res = true;
+  }
+
+  return res;
+}
+
+type PokemonProps = {
+  nickname: string;
+  name: string;
+  type: Array<{
+    name: string;
+    url: string;
+  }>;
+  sprite: string;
+  moves: Array<string>;
+  color: string;
+};
+
+export function savePokemon(pokemon: PokemonProps): boolean {
+  let res = false;
+  const list_pocket = localStorage.getItem("list_pocket");
+
+  if (list_pocket === null) {
+    localStorage.setItem("list_pocket", JSON.stringify([pokemon]));
+    res = true;
+  } else {
+    const list_pokemon: Array<PokemonProps> = JSON.parse(list_pocket);
+    const checkPokemonAndNickname: number = list_pokemon.findIndex(
+      (list) => list.name !== pokemon.name && list.nickname !== pokemon.nickname
+    );
+
+    if (checkPokemonAndNickname === -1) {
+      list_pokemon.push(pokemon)
+      localStorage.setItem("list_pocket", JSON.stringify(list_pokemon));
+      res = true
+    }
+  }
+
+  return res;
+}
