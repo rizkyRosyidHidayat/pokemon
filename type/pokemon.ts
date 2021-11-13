9891750668180f57bb5b03e75191d6490064c5a9
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-11-13 22:55:54 +0700

    add type

---
+++ type/pokemon.ts
@@ -0,0 +1,11 @@
+export interface  MyPokemonProps {
+  nickname: string;
+  name: string;
+  type: Array<{
+    name: string;
+    url: string;
+  }>;
+  sprite: string;
+  moves: Array<string>;
+  color: string;
+};
